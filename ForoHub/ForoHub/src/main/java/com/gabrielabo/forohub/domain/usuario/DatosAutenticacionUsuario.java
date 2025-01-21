0eddf0230c34d070e6af1fd45cb6b56f10bcb6ff
Gabriela Bo <hikariodagled@gmail.com>
2025-01-20 20:51:55 -0600

    Primer commit

---
+++ ForoHub/ForoHub/src/main/java/com/gabrielabo/forohub/domain/usuario/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package com.gabrielabo.forohub.domain.usuario;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+}
---
+++ ForoHub/ForoHub/src/main/java/com/gabrielabo/forohub/dto/DatosActualizarTopico.java
@@ -0,0 +1,11 @@
+package com.gabrielabo.forohub.dto;
+
+public record DatosActualizarTopico(
+        Long id,
+        String titulo,
+        String mensaje,
+//        LocalDateTime fecha_Creacion,
+//        Boolean status,
+        String autor,
+        String curso) {
+}
